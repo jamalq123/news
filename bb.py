f0ccac3490ea86a4dcfb3798fe722c22fa18d544
jamalq123 <jamal_qamar@hotmail.com>
2023-12-29 18:40:31 +0500

    Update bb.py

--- bb.py
+++ bb.py
@@ -2,23 +2,20 @@ import streamlit as st
 from newspaper import Article
 from googletrans import Translator
 import nltk
-
 import os
-
-# Define NLTK data path using Streamlit environment variables
-st.set_option('deprecation.showfileUploaderEncoding', False)
-
-# Set NLTK data path to ensure access to the 'punkt' tokenizer
-nltk.data.path.append(os.path.join(st.__path__[0], 'nltk_data'))
 
 # Download 'punkt' tokenizer if not already downloaded
 nltk.download('punkt', quiet=True)
 
-# Set NLTK data path to ensure access to the 'punkt' tokenizer
-nltk.data.path.append("/path/to/your/nltk_data")
+# Function to set NLTK data path using Streamlit environment variables
+def set_nltk_data_path():
+    st.set_option('deprecation.showfileUploaderEncoding', False)
+    nltk.data.path.append(os.path.join(st.__path__[0], 'nltk_data'))
 
 # Function to extract and display article details
 def extract_article_details(article_url, target_language):
+    set_nltk_data_path()  # Set NLTK data path
+
     article = Article(article_url)
     article.download()
     article.parse()
@@ -27,47 +24,14 @@ def extract_article_details(article_url, target_language):
     
     st.header("Article Details")
     
-    # Display complete text
-    st.subheader("Complete Text")
-    st.write(article.text)
-
-    # Display keywords
-    st.subheader("Keywords")
-    st.write(", ".join(article.keywords))
-
-    # Display article summary
-    st.header("Article Summary")
-    st.subheader(article.title)
-    st.write(article.summary)
-
-    # Translate content if a target language is selected
-    if target_language != "Original" and target_language is not None:
-        if article.text:  # Check if the article text is not empty or None
-            translated_title = translator.translate(article.title, dest=target_language).text
-            translated_text = translator.translate(article.text, dest=target_language).text
-            translated_summary = translator.translate(article.summary, dest=target_language).text
-
-            st.header(f"Translated Content ({target_language})")
-            st.subheader("Translated Title")
-            st.write(translated_title)
-            
-            st.subheader("Translated Text")
-            st.write(translated_text)
-            
-            st.subheader("Translated Summary")
-            st.write(translated_summary)
-        else:
-            st.warning("No text available for translation.")
-    else:
-        st.info("Select a target language to translate.")
+    # Rest of your code for displaying article details and translation...
 
 # Streamlit app
 def main():
     st.title("Article Translator")
 
-    # Input for article link and language selection
-    article_link = st.text_input("Enter the article link:")
-    target_language = st.selectbox("Select Target Language", ["Original", "Spanish", "French", "German", "Italian", "Urdu"])
+    article_link = st.text_input("Enter the URL of the article:")
+    target_language = st.selectbox("Select a target language:", ["Original", "Spanish", "French", "German", "Italian", "Urdu"])
 
     if st.button("Translate"):
         if article_link:
@@ -76,6 +40,6 @@ def main():
             except Exception as e:
                 st.error(f"Error: Unable to analyze the article. Please check the link. Exception: {e}")
                 st.exception(e)
-    	
+
 if __name__ == "__main__":
     main()
