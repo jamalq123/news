8b936a1acece5dd75d7b944353b4ae428b457427
jamalq123 <jamal_qamar@hotmail.com>
2023-12-29 15:30:53 +0500

    Update bb.py

--- bb.py
+++ bb.py
@@ -1,6 +1,7 @@
 import streamlit as st
-import nltk
 from newspaper import Article
+from googletrans import Translator
+import nltk
 
 # Download 'punkt' tokenizer if not already downloaded
 nltk.download('punkt', quiet=True)
@@ -9,12 +10,14 @@ nltk.download('punkt', quiet=True)
 nltk.data.path.append("/path/to/your/nltk_data")
 
 # Function to extract and display article details
-def extract_article_details(article_url):
+def extract_article_details(article_url, target_language):
     article = Article(article_url)
     article.download()
     article.parse()
     article.nlp()
 
+    translator = Translator()
+    
     st.header("Article Details")
     
     # Display complete text
@@ -30,17 +33,34 @@ def extract_article_details(article_url):
     st.subheader(article.title)
     st.write(article.summary)
 
+    # Translate content if a target language is selected
+    if target_language != "Original" and target_language is not None:
+        translated_title = translator.translate(article.title, dest=target_language).text
+        translated_text = translator.translate(article.text, dest=target_language).text
+        translated_summary = translator.translate(article.summary, dest=target_language).text
+
+        st.header(f"Translated Content ({target_language})")
+        st.subheader("Translated Title")
+        st.write(translated_title)
+        
+        st.subheader("Translated Text")
+        st.write(translated_text)
+        
+        st.subheader("Translated Summary")
+        st.write(translated_summary)
+
 # Streamlit app
 def main():
-    st.title("Article Analyzer")
+    st.title("Article Translator")
 
-    # Input for article link
+    # Input for article link and language selection
     article_link = st.text_input("Enter the article link:")
+    target_language = st.selectbox("Select Target Language", ["Original", "Spanish", "French", "German", "Italian", "Urdu"])
 
-    if st.button("Analyze"):
+    if st.button("Translate"):
         if article_link:
             try:
-                extract_article_details(article_link)
+                extract_article_details(article_link, target_language)
             except Exception as e:
                 st.error(f"Error: Unable to analyze the article. Please check the link. Exception: {e}")
                 st.exception(e)
