4aae107704fe7e6d6af6ad46c830e4c5bb016a57
jamalq123 <jamal_qamar@hotmail.com>
2023-12-29 14:15:38 +0500

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -1,16 +1,13 @@
 import streamlit as st
 from newspaper import Article
-from googletrans import Translator
 
 # Function to extract and display article details
-def extract_article_details(article_url, target_language):
+def extract_article_details(article_url):
     article = Article(article_url)
     article.download()
     article.parse()
     article.nlp()
 
-    translator = Translator()
-    
     st.header("Article Details")
     
     # Display complete text
@@ -26,36 +23,19 @@ def extract_article_details(article_url, target_language):
     st.subheader(article.title)
     st.write(article.summary)
 
-    # Translate content if a target language is selected
-    if target_language != "Original" and target_language is not None:
-        translated_title = translator.translate(article.title, dest=target_language).text
-        translated_text = translator.translate(article.text, dest=target_language).text
-        translated_summary = translator.translate(article.summary, dest=target_language).text
-
-        st.header(f"Translated Content ({target_language})")
-        st.subheader("Translated Title")
-        st.write(translated_title)
-        
-        st.subheader("Translated Text")
-        st.write(translated_text)
-        
-        st.subheader("Translated Summary")
-        st.write(translated_summary)
-
 # Streamlit app
 def main():
-    st.title("Article Translator")
+    st.title("Article Analyzer")
 
-    # Input for article link and language selection
+    # Input for article link
     article_link = st.text_input("Enter the article link:")
-    target_language = st.selectbox("Select Target Language", ["Original", "Spanish", "French", "German", "Italian", "Urdu"])
 
-    if st.button("Translate"):
+    if st.button("Analyze"):
         if article_link:
             try:
-                extract_article_details(article_link, target_language)
+                extract_article_details(article_link)
             except Exception as e:
-                st.error("Error: Unable to translate the article. Please check the link.")
+                st.error("Error: Unable to analyze the article. Please check the link.")
 
 if __name__ == "__main__":
     main()
